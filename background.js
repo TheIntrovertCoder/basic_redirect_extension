5cb339bb6883dda9bbab01562a670a5118abb6dd
TheIntrovertCoder <yogeshbabu231@proton.me>
2022-12-14 07:46:35 -0700

    Nothing to see -> Added google.com just because people are used to doing that on a blank page -> Add commonly typed shorthand for reddit and youtube to make redirect a bit faster

--- background.js
+++ background.js
@@ -4,16 +4,15 @@ let browser = () => {
 
 browser().webRequest.onBeforeRequest.addListener(
     (details) => {
-
-        let youtube_links = [
+        const youtube_links = [
             "youtu.be",
+            "youtube.com",
             "www.youtube.com",
             "www.youtube-nocookie.com",
         ];
 
         let hostname = details.url.split("/")[2];
 
-        // TODO: Make GUI to let users select their prefered instances
         for (let i = 0; i <= youtube_links.length - 1; i++) {
             if (hostname === youtube_links[i])
                 return {
@@ -24,12 +23,17 @@ browser().webRequest.onBeforeRequest.addListener(
                 };
         }
 
-        if (hostname === "www.reddit.com")
+        if (hostname === "www.reddit.com" || hostname === "reddit.com")
             return {
                 redirectUrl: details.url.replace(
                     hostname,
                     "libreddit.spike.codes"
                 ),
+            };
+
+        if (hostname === "www.google.com")
+            return {
+                redirectUrl: details.url.replace(hostname, "search.brave.com"),
             };
     },
     { urls: ["<all_urls>"] },
